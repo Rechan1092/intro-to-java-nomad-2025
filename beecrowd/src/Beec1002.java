87e333b589e3ebce7e0a2eebe94203fa8a7c45ee
Rechan <saykalsaykal@gmail.com>
2025-09-23 11:38:07 +0600

    beec 1000-3

---
+++ beecrowd/src/Beec1002.java
@@ -0,0 +1,10 @@
+
+import java.util.Scanner;
+
+public class Beec1002 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double r = sc.nextInt();
+        System.out.printf("A = %.4f", (Math.PI * r * r));
+    }
+}
